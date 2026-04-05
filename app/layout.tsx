4d2e7a1b027881c596a641edd38d16893c2f53d5
Rodrigo de Jesus Macedo <roromacedo1207@gmail.com>
2026-04-05 18:20:12 -0300

    fix: adiciona ThemeProvider e UserProvider ao layout raiz

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/_context/themeProvider";
+import { UserProvider } from "@/_context/userContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+          <ThemeProvider>
+            <UserProvider>
+              {children}
+            </UserProvider>
+          </ThemeProvider>
+        </body>
     </html>
   );
 }
